285fcd8a3136eb5ab773631c7b59c124dbb480d8
Giancarlo Frix <gfrix98@gmail.com>
2015-05-16 18:02:32 -0400

    added PositionedObject

---
+++ src/main/java/com/agarextend/app/PositionedObject.java
@@ -0,0 +1,41 @@
+package com.agarextend.app;
+
+public abstract class PositionedObject {
+   private double x;
+   private double y;
+
+   public PositionedObject() {
+      this.x = 0;
+      this.y = 0;
+   }
+
+   public PositionedObject(double x, double y) {
+      this.x = x;
+      this.y = y;
+   }
+
+   public double getX() {
+      return x;
+   }
+
+   public double incX(double i) {
+      x += i;
+   }
+   public void setX(double x) {
+      this.x = x;
+   }
+
+   public double getY() {
+      return y;
+   }
+
+   public void setY(double y) {
+      this.y = y;
+   }
+
+   public double incY(double i) {
+      y += i;
+   }
+
+   public abstract void move();
+}
