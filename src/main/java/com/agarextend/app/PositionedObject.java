5e98b18232e286969c18084706c2ff1ceb79c626
Giancarlo Frix <gfrix98@gmail.com>
2015-05-16 20:52:18 -0400

    clean up

--- src/main/java/com/agarextend/app/PositionedObject.java
+++ src/main/java/com/agarextend/app/PositionedObject.java
@@ -3,11 +3,6 @@ package com.agarextend.app;
 
 import java.awt.*;
 
-/* 
-   I added sides, mass, xcorners, and ycorners to help me draw 
-*/
-
-
 
 public abstract class PositionedObject {
    private int x;
@@ -23,7 +18,7 @@ public abstract class PositionedObject {
       this.mass = 0;
    }
 //default
-   public PositionedObject(double x, double y) {
+   public PositionedObject(int x, int y) {
       this.x = x;
       this.y = y;
       this.sides = 3;
@@ -31,7 +26,7 @@ public abstract class PositionedObject {
    }
    
 //barely used   
-   public PositionedObject(double x, double y, int sides, int mass) {
+   public PositionedObject(int x, int y, int sides, int mass) {
       this.x = x;
       this.y = y;
       this.sides = sides;
