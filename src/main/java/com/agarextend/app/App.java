6710cc4ad886b7c396ff7fb70f000a1033198713
Giancarlo Frix <gfrix98@gmail.com>
2015-05-17 02:16:38 -0400

    fixed it so it runs at all

--- src/main/java/com/agarextend/app/App.java
+++ src/main/java/com/agarextend/app/App.java
@@ -1,9 +1,19 @@
 package com.agarextend.app;
 
+import javax.swing.*;
+
 public class App
 {
+   private static final int SIZE = 1000;
     public static void main( String[] args )
     {
-        GameFrame frame = new GameFrame("Agar IMproved");
+       JFrame frame = new JFrame("Agar IMproved");
+       frame.setSize(SIZE, SIZE);
+       GamePanel mainPanel = new GamePanel(SIZE);
+       mainPanel.addMouseMotionListener(mainPanel);
+       //this.setContentPane(mainPanel);
+       frame.add(mainPanel);
+       frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+       frame.setVisible(true);
     }
 }
--- src/main/java/com/agarextend/app/GameFrame.java
+++ src/main/java/com/agarextend/app/GameFrame.java
@@ -3,15 +3,4 @@ package com.agarextend.app;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
-   private final int SIZE = 1000;
-   public GameFrame(String s) {
-      super(s);
-      GamePanel mainPanel = new GamePanel(SIZE);
-      this.setSize(SIZE, SIZE);
-      this.addMouseMotionListener(mainPanel);
-      //this.setContentPane(mainPanel);
-      this.add(mainPanel);
-      this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-      this.setVisible(true);
-   }
 }
