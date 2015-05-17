850e62c28849789cafb6c8f8c919d4527408b439
Voided Warranty <fiddlerdude@hotmail.com>
2015-05-17 00:02:10 -0400

    Added Herbivore, no ai

---
+++ src/main/java/com/agarextend/app/Herbivore.java
@@ -0,0 +1,21 @@
+package com.agarextend.app;
+public class Herbivore extends Movable
+{
+   public Herbivore() {
+      super();
+   }
+   
+   public Herbivore(int xC, int yC, int dir, double dist, int m, double s)
+   {
+      setX(xC);
+      setY(yC);
+      setDirection(dir);
+      setPointerDist(dist);
+      setMass(m);
+      setSpeed(s);
+   }
+
+   public void display() {
+
+   }
+}   
