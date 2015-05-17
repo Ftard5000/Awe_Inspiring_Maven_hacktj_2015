8e3b165c274b3fa91fde3b4a9b7fadfac7d40815
Voided Warranty <fiddlerdude@hotmail.com>
2015-05-17 05:54:47 -0400

    Avoid boundaries, constantly spawn pellets

--- src/main/java/com/agarextend/app/Main.java
+++ src/main/java/com/agarextend/app/Main.java
@@ -92,7 +92,7 @@ public class Main extends JPanel  implements MouseMotionListener, Runnable {
                 
                 
          if(mousePoint != null) {
-                                
+                                   
             double Xdif = playerLocation.getX() - mousePoint.getX();
             double Ydif = playerLocation.getY() - mousePoint.getY();
             double angle = Math.atan2(Ydif, Xdif) * 180 / Math.PI;
@@ -105,9 +105,11 @@ public class Main extends JPanel  implements MouseMotionListener, Runnable {
                                                 ? playerLocation.getY()-dY : mousePoint.getY();
             playerLocation.setLocation(newX, newY);
             for(Herbivore h : herbivores)
-               h.move();
+               h.choseMove();
             for(Predator p : predators)
-               p.move();
+               p.choseMove();
+            if(Math.random()<.01)
+               pellets.add(new Pellet());
             repaint();
             try {
                Thread.sleep(10);
